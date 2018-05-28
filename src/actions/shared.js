fc565b8d4573ac0cb5b76f72a7d5e274260a0d15
Paul <peng-ling@users.noreply.github.com>
2018-05-28 09:49:30 +0200

    trainride end commit;

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,14 +1,17 @@
 import { getInitialData } from '../utils/api';
 import { receiveUsers } from '../actions/users';
 import { receiveTweets } from '../actions/tweets';
+import { setAuthedUser } from '../actions/authedUser';
+
+const AUTHED_ID = 'tylermcginis';
 
 export function handleInitialData() {
   return(dispatch) => {
     return getInitialData()
       .then (({ users, tweets }) => {
-                dispatch(receiveUsers(users));
+        dispatch(receiveUsers(users));
         dispatch(receiveTweets(tweets));
-
+        dispatch(setAuthedUser(AUTHED_Id));
       });
   };
 }
